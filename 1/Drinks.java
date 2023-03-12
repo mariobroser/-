fca06c3aa5f0aa8cdeafc09cec42642473705e83
mariobroser <55622231+mariobroser@users.noreply.github.com>
2023-03-12 17:10:10 +0500

    Add files via upload

---
+++ 1/Drinks.java
@@ -0,0 +1,19 @@
+public class Drinks extends Goods {
+    private double vol;
+
+    public Drinks(String name, double price, double amount, String unit, double vol) {
+        super(name, price, amount, unit);
+        this.vol = vol;
+    }
+
+    public double getVolume() {
+        return this.vol;
+    }
+
+    @Override
+    public String toString() {
+        String result = String.format("%s\nОбъем: %.2f", super.toString(), this.getVolume());
+        return result;
+    }
+
+}
